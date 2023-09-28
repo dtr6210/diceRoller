74c22e6d74bfaa57650a7c211bd7daa28fbd8476
dtr6210 <rialsdean@gmail.com>
2023-09-27 20:26:42 -0500

    pushing latest changes to function branch before create new

--- dice.js
+++ dice.js
@@ -1,4 +1,8 @@
 
+function diceRoll(num, num2, Name1) {
+    console.log("this is how many sides:", num, num2)
+    document.getElementById(Name1).innerHTML=numberMinMax(num, num2)
+}
 
 function d6(){
     console.log("button clicked")
@@ -9,7 +13,7 @@ function d6(){
 function d12(){
     console.log("button clicked")
     console.log("this is the d12 dice")
-    numberMinMax(1,8) // calls numberMinMax function defined below
+    numberMinMax(1,12) // calls numberMinMax function defined below
 }
 function numberMinMax(min,max){
     const diceRoll=Math.floor(Math.random() * (max - min +1)) + min
